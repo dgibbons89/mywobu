5854326665e95a2d216a2268c27892b6dbc03fb0
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-08 12:31:24 -0400

    omni.rb file change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '506537142787997', '865dccb165d454810738fa14a63c20db',
-  :scope => 'email, offline_access, user_birthday, user_friends, read_friendlists, friends_birthday' {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  :scope => 'email, offline_access, user_birthday, user_friends, read_friendlists, friends_birthday', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
